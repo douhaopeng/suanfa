c2344f2ff9287728c2276a3bd065ad1f1f48d98b
douhaopeng <1591994251@qq.com>
2020-04-22 14:56:25 +0800

    22

---
+++ 279_2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numSquares(int n) {
+
+      Set<Integer> seen = new HashSet<>();
+      int depth = 0;
+      Queue<Integer> queue = new LinkedList<>();
+      queue.offer(n);
+      while(!queue.isEmpty()){
+        int size = queue.size();
+        depth++;
+        for(int i =0; i<size;i++){
+          int node =  queue.poll();
+          for(int j=1;j<=Math.sqrt(node);j++){
+            int  les  =  node - j*j;
+            if(les == 0){
+              return depth;
+            }
+            queue.offer(les);
+          }
+        }
+      }
+      return 0;
+    }
+}
