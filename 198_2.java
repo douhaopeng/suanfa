0c814ba07f1221c62fcf0d4c6cce9840e64e2964
douhaopeng <1591994251@qq.com>
2020-04-24 16:35:15 +0800

    22

---
+++ 198_2.java
@@ -0,0 +1,15 @@
+算法思路为第i-2个数加上最后一个数的和与倒数第二个数比大小
+class Solution{
+
+public int rob(int[] nums){
+if(nums.length == 0 ) return 0 ;
+if(nums.length ==1) return nums[0];
+int[] dp  =  new int[nums.length];
+dp[0] = nums[0];
+dp[1] = Math.max(nums[0],nums[1]);
+for (int i = 2;i< nums.length ;i++ ) {
+  dp[i] = Math.max(dp[i-2]+nums[i],dp[i-1]);
+}
+return dp[nums.length-1];
+}
+}
